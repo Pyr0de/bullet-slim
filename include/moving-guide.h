c258326d751461cf76deb62e8611cb34a56003af
Pyrode <pyrode.01@gmail.com>
2024-07-28 21:31:15 +0530

    Added rendering for guide

--- include/moving-guide.h
+++ include/moving-guide.h
@@ -5,15 +5,16 @@ class MovingGuide {
 	int dir = 1;
 	float i = 0, speed = 0;
 	std::vector<int> pX, pY;
+	float currSticky = 0;
 
 	void test(SDL_Renderer *renderer);
 	public:
 
-		MovingGuide(int x1, int y1, int x2, int y2, float speed);
+	MovingGuide(int x1, int y1, int x2, int y2, float speed);
 
-		void getCoords(int &x, int &y);
+	void getCoords(int &x, int &y);
 
-		void tick(double deltaTime);
+	void tick(double deltaTime);
 
-		void render(SDL_Renderer *renderer);
+	void render(SDL_Renderer *renderer);
 };
--- src/moving-guide.cpp
+++ src/moving-guide.cpp
@@ -1,7 +1,22 @@
 #include "moving-guide.h"
+#include "texture.h"
+#include <SDL_rect.h>
 #include <SDL_render.h>
 #include <cmath>
 #include <cstdio>
+
+#define GUIDE_SIZE 14
+
+Texture *guide_tex = nullptr;
+Texture *guide_end_tex = nullptr;
+
+void loadGuideTextures(SDL_Renderer *renderer) {
+	guide_tex = new Texture();
+	guide_end_tex = new Texture();
+
+	guide_tex->loadFile(renderer, "assets/guide.png");
+	guide_end_tex->loadFile(renderer, "assets/guide_end.png");
+}
 
 MovingGuide::MovingGuide(int x1, int y1, int x2, int y2, float s) {
 	speed = s;
@@ -23,7 +38,7 @@ MovingGuide::MovingGuide(int x1, int y1, int x2, int y2, float s) {
 		x += xin;
 		y += yin;
 	}
-
+	angle = std::atan2(dy,dx);
 }
 
 void MovingGuide::tick(double deltaTime) {
@@ -35,8 +50,33 @@ void MovingGuide::tick(double deltaTime) {
 }
 
 void MovingGuide::render(SDL_Renderer *renderer) {
-	
+	if (guide_tex == nullptr) {
+		loadGuideTextures(renderer);
+	}
+	auto tex_half = GUIDE_SIZE/2;
 
+	guide_tex->anchor = {tex_half, tex_half};
+	guide_end_tex->anchor = {tex_half, tex_half};
+
+	SDL_Rect rend_box = {300,300, GUIDE_SIZE, GUIDE_SIZE};
+
+	guide_tex->setRotation(angle * 180/M_PI);
+
+	for (int i = 0; i < pX.size(); i += tex_half) {
+		rend_box.x = pX[i] - tex_half;
+		rend_box.y = pY[i] - tex_half;
+		
+		auto tex = guide_tex;
+		if (i == 0 || i + tex_half >= pX.size()) {
+			guide_end_tex->setRotation(angle * 180/M_PI);
+			if (i != 0) {
+				guide_end_tex->anchor = { tex_half, tex_half};
+				guide_end_tex->setRotation(angle * 180/M_PI + 180);
+			}
+			tex = guide_end_tex;
+		}
+		tex->scaleAndRender(renderer, &rend_box);
+	}
 
 #ifdef __DEBUG__
 	test(renderer);
@@ -45,7 +85,7 @@ void MovingGuide::render(SDL_Renderer *renderer) {
 
 
 void MovingGuide::test(SDL_Renderer *renderer) {
-	SDL_SetRenderDrawColor(renderer, 0,255,255, 255);
+	SDL_SetRenderDrawColor(renderer, 0,0,0, 255);
 	SDL_RenderDrawLine(renderer, pX[0], pY[0],pX[pX.size() - 1], pY[pY.size() - 1]);
 
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
