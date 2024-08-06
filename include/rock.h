dc0d8d9f572ab983089b1bec8602057eb8bd0dfd
Pyrode <pyrode.01@gmail.com>
2024-08-06 22:42:57 +0530

    Added grounded field for rock

--- include/rock.h
+++ include/rock.h
@@ -4,13 +4,14 @@
 #include <SDL_render.h>
 #include <vector>
 class Rock {
-	public:
 
+	public:
+	bool grounded = false;
 	SDL_Rect hitbox = {0,0,0,0};
 
 	Rock(int x, int y);
 
-	void tick(double deltaTime, std::vector<SDL_Rect*> &obstacles);
+	bool tick(double deltaTime, std::vector<SDL_Rect*> &obstacles);
 
 	void render(SDL_Renderer* render);
 	void test(SDL_Renderer* render);
--- src/rock.cpp
+++ src/rock.cpp
@@ -14,14 +14,21 @@ Rock::Rock(int x, int y) {
 	hitbox.h = 32;
 }
 
-void Rock::tick(double deltaTime, std::vector<SDL_Rect*> &obstacles) {
+bool Rock::tick(double deltaTime, std::vector<SDL_Rect*> &obstacles) {
+	if (grounded) {
+		return false;
+	}
+
 	hitbox.y += FALL_SPEED * deltaTime;
 	for (SDL_Rect* i: obstacles) {
 		if (checkCollision(&hitbox, i)) {
 			hitbox.y = i->y - hitbox.h;
+			grounded = true;
 			break;
 		}
 	}
+
+	return false;
 }
 
 void Rock::render(SDL_Renderer* render) {
@@ -34,6 +41,4 @@ void Rock::render(SDL_Renderer* render) {
 void Rock::test(SDL_Renderer* render) {
 	SDL_SetRenderDrawColor(render, 0,255,255,255);
 	SDL_RenderDrawRect(render, &hitbox);
-	SDL_SetRenderDrawColor(render, 0,255,255,255);
-	SDL_RenderDrawLine(render, 100, 100, 120,100);
 }
