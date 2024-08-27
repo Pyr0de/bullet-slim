f47720d2da07099578e466972919aafe525d9c88
Pyrode <pyrode.01@gmail.com>
2024-08-27 19:16:27 +0530

    Added favicon and pause , fullscreen buttons

--- web/index.js
+++ web/index.js
@@ -21,16 +21,26 @@ window.oncontextmenu = function(event) {
 if (!mobileAndTabletCheck()) {
 	document.getElementById("buttons").remove()
 }else {
+	document.getElementById('up').addEventListener("touchstart", () => {handleControl(2,1)})
+	document.getElementById('up').addEventListener("touchend", () => {handleControl(2,0)})
 
-document.getElementById('up').addEventListener("touchstart", () => {handleControl(2,1)})
-document.getElementById('up').addEventListener("touchend", () => {handleControl(2,0)})
+	document.getElementById('left').addEventListener("touchstart", () => {handleControl(0,1)})
+	document.getElementById('left').addEventListener("touchend", () => {handleControl(0,0)})
 
-document.getElementById('left').addEventListener("touchstart", () => {handleControl(0,1)})
-document.getElementById('left').addEventListener("touchend", () => {handleControl(0,0)})
+	document.getElementById('right').addEventListener("touchstart", () => {handleControl(1,1)})
+	document.getElementById('right').addEventListener("touchend", () => {handleControl(1,0)})
 
-document.getElementById('right').addEventListener("touchstart", () => {handleControl(1,1)})
-document.getElementById('right').addEventListener("touchend", () => {handleControl(1,0)})
+	document.getElementById('interact').addEventListener("touchstart", () => {handleControl(3,1)})
+	document.getElementById('interact').addEventListener("touchend", () => {handleControl(3,0)})
 
-document.getElementById('interact').addEventListener("touchstart", () => {handleControl(3,1)})
-document.getElementById('interact').addEventListener("touchend", () => {handleControl(3,0)})
+	document.getElementById('pause').addEventListener("touchstart", () => {handleControl(4,1)})
+	document.getElementById('pause').addEventListener("touchend", () => {handleControl(4,0)})
+
+	document.getElementById('fullscreen').addEventListener("touchstart", () => {
+		if (window.innerWidth != screen.width || window.innerHeight != screen.height) {
+			document.exitFullscreen();
+		}else {
+			document.documentElement.requestFullscreen();
+		}
+	})
 }
